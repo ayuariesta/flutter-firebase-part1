05f5a8460107ad7b95591dfefc6abf1a76ba1600
ayuariesta <=>
2022-09-17 08:25:03 +0700

    Set up Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,10 @@ import 'app.dart';
 const clientId = 'YOUR_CLIENT_ID';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
